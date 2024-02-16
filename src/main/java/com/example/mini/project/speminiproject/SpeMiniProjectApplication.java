e0ae82d82cdf8f3f0736bfd05c3da8f3643e5e9d
abhisheksharma402 <abhib12402@gmail.com>
2024-02-16 11:01:22 +0530

    initial setup

---
+++ src/main/java/com/example/mini/project/speminiproject/SpeMiniProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.mini.project.speminiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeMiniProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeMiniProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mini/project/speminiproject/SpeMiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mini.project.speminiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeMiniProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
