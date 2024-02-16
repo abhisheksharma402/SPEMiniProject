dcc5f8a729f74f3d11e373880e8d189f1a0dcc47
abhisheksharma402 <abhib12402@gmail.com>
2024-02-16 17:42:07 +0530

    changed

--- src/main/java/com/example/mini/project/speminiproject/SpeMiniProjectApplication.java
+++ src/main/java/com/example/mini/project/speminiproject/SpeMiniProjectApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpeMiniProjectApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpeMiniProjectApplication.class, args);
+//        SpringApplication.run(SpeMiniProjectApplication.class, args);
+
+        System.out.println("hello");
+
     }
 
 }
